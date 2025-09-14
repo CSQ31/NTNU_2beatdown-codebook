e53c0629fbeae015463a2979b2f5d253b44d5880
CSQ <csqsaysuwu@gmail.com>
2025-09-15 02:11:13 +0800

    Initial draft, removed alot of stuff

--- content/data-structures/FenwickTree.h
+++ content/data-structures/FenwickTree.h
@@ -1,34 +1,34 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-30
+ * Author: Chew Shao Qian
+ * Date: 2025-09-08
  * License: CC0
- * Source: folklore/TopCoder
- * Description: Computes partial sums a[0] + a[1] + ... + a[pos - 1], and updates single elements a[i],
- * taking the difference between the old and new value.
+ * Source: folklore
+ * Description: One-indexed fenwick tree
  * Time: Both operations are $O(\log N)$.
  * Status: Stress-tested
  */
 #pragma once
 
-struct FT {
-	vector<ll> s;
-	FT(int n) : s(n) {}
-	void update(int pos, ll dif) { // a[pos] += dif
-		for (; pos < sz(s); pos |= pos + 1) s[pos] += dif;
+struct fenwick{
+	int n;
+	vector<ll>bit;
+	void upd(int pos,ll val){
+		for(int i=pos;i<=n;i+=i&(-i))bit[i]+=val;
 	}
-	ll query(int pos) { // sum of values in [0, pos)
+	ll query(int r){
 		ll res = 0;
-		for (; pos > 0; pos &= pos - 1) res += s[pos-1];
-		return res;
+		for(int i=r;i>0;i-=i&(-i))res+=bit[i];
+	    return res;
 	}
-	int lower_bound(ll sum) {// min pos st sum of [0, pos] >= sum
-		// Returns n if no sum is >= sum, or -1 if empty sum is.
-		if (sum <= 0) return -1;
-		int pos = 0;
-		for (int pw = 1 << 25; pw; pw >>= 1) {
-			if (pos + pw <= sz(s) && s[pos + pw-1] < sum)
-				pos += pw, sum -= s[pos-1];
+	int search(ll val){
+		ll sum = 0,pos = 0;
+		for(int i=19;i>=0;i--){
+			if(pos+(1<<i) < n && sum+bit[pos+(1<<i)]<val){
+				pos+=(1<<i);
+				sum+=bit[pos];
+			}
 		}
-		return pos;
+		return pos+1;
 	}
+	fenwick(int _n):n(_n),bit(vector<ll>(n+1,0)){}
 };
