50ec5fab24b2c4c9ba628350ebff3bf38957e19d
Din <din@DindeMacBook-Air.local>
2025-09-19 14:43:30 +0800

    update template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -4,11 +4,13 @@ using namespace std;
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
+#define fi first
+#define se second
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int main() {
+signed main() {
 	cin.tie(0)->sync_with_stdio(0);
 	cin.exceptions(cin.failbit);
 }
