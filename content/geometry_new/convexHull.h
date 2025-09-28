f89580a09cce1e3da2b39fab86f777f4f562c77f
Din <din@DindeMacBook-Air.local>
2025-09-28 11:25:41 +0800

    add convexHull.h

---
+++ content/geometry_new/convexHull.h
@@ -0,0 +1,30 @@
+/**
+ * Author: Stjepan Glavina, chilli
+ * Date: 2019-05-05
+ * License: Unlicense
+ * Source: https://github.com/stjepang/snippets/blob/master/convex_hull.cpp
+ * Description:
+\\\begin{minipage}{75mm}
+Returns a vector of the points of the convex hull in counter-clockwise order.
+Points on the edge of the hull between two other points are not considered part of the hull.
+\end{minipage}
+\begin{minipage}{15mm}
+\vspace{-6mm}
+\includegraphics[width=\textwidth]{content/geometry/ConvexHull}
+\vspace{-6mm}
+\end{minipage}
+ * Time: O(n \log n)
+ * Status: stress-tested, tested with kattis:convexhull
+*/
+v<pt> convexHull(v<pt> pts) {
+	if (sz(pts) <= 1) return pts;
+	sort(all(pts), [&](pt& a, pt& b) { return make_pair(a.x, a.y) < make_pair(b.x, b.y); } );
+	v<pt> h(2*sz(pts)+1);
+	int s = 0, t = 0;
+	for (int it = 2; it--; s = --t, reverse(all(pts)))
+		for (pt p : pts) {
+			while (t >= s + 2 && orient(h[t-2], h[t-1], p) < 0) t--;
+			h[t++] = p; 
+		}
+	return {h.begin(), h.begin() + t - (t == 2 && h[0] == h[1])};
+}
